d80848ea203f72b8a10f11f7ab116a77b516ff46
nicolo <zenari.nico@gmail.com>
2018-07-11 11:15:59 +0200

    Untokenize

--- tokenizer.py
+++ tokenizer.py
@@ -1,7 +1,14 @@
 import tokenize
 
 
-def tokenize_file(file_source):
-    file_obj = open(file_source, 'r')
+def tokenize_file(source_file):
+    file_obj = open(source_file, 'r')
     tokens = [list(a) for a in tokenize.generate_tokens(file_obj.readline)]
+    file_obj.close()
     return tokens
+
+
+def untokenize_to_file(tokens, destination_file='./out.py'):
+    file_obj = open(destination_file)
+    file_obj.write(tokenize.untokenize(tokens))
+    file_obj.close()
