b66c7bb99d44a88cd7cbb44afb30181ad372985b
nicolo <zenari.nico@gmail.com>
2018-07-11 11:54:11 +0200

    update tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -1,14 +1,22 @@
 import tokenize
+import io
+
+
+def tokenize_line(line):
+    return [list(a) for a in tokenize.generate_tokens(io.StringIO(line).readline())]
+
+
+def untokenize_line(tokens):
+    return tokenize.untokenize(tokens)
 
 
 def tokenize_file(source_file):
-    file_obj = open(source_file, 'r')
-    tokens = [list(a) for a in tokenize.generate_tokens(file_obj.readline)]
-    file_obj.close()
-    return tokens
+    with open(source_file, 'r') as file_obj:
+        return file_obj.readlines()
 
 
-def untokenize_to_file(tokens, destination_file='./out.py'):
-    file_obj = open(destination_file)
-    file_obj.write(tokenize.untokenize(tokens))
-    file_obj.close()
+def untokenize_to_file(lines, destination_file='./out.py'):
+    # TODO: check if it works
+    with open(destination_file, 'w') as file_obj:
+        for line in lines:
+            file_obj.writable(line)
