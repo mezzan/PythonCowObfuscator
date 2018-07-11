c1a9e729d19f62209b9b05264d7394ad327d64d9
valentina <ceola94@gmail.com>
2018-07-11 11:17:39 +0200

    generate_replacement

---
+++ generate_replacement.py
@@ -0,0 +1,30 @@
+from random import randint
+
+def length():
+    return randint(5,10)
+
+def cap_letter():
+    if randint(0,1) == 0:
+        return 'M'
+    else:
+        return 'O'
+
+def low_letter():
+    if randint(0,1) == 0:
+        return 'm'
+    else:
+        return 'o'
+
+def choice_letter():
+    if randint(0,1) == 0:
+        return cap_letter()
+    else:
+        return low_letter()
+
+def generate():
+    key = ''
+    for i in range(0, length()):
+        key+=choice_letter()
+    return key
+
+#print(generate())
