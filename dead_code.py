8c5f955d42cb1437ada9f371504ca8d3124808ec
mezzan <m.zanotti.1992@gmail.com>
2018-07-16 21:37:43 +0200

    dead code

--- dead_code.py
+++ dead_code.py
@@ -2,12 +2,12 @@ import random
 import tokenize
 import string
 
-list=['while', 'for', 'def', 'if']
+candidate_lines=['while', 'for', 'def', 'if ']
 
-
-def control(source_string):
-	for x in list:
-		if x in source_string:
+#funzione che veriifica se la riga è una riga candidata
+def is_candidate(source_string):
+	for line in candidate_lines:
+		if line in source_string:
 			return True
 		
 	return False
@@ -19,70 +19,66 @@ def start(source_path):
 
 	lines = source.readlines()
 
-	#variable_is_inizialized = False
-
 	dead_code_variables = open('./dead_code/dead_code_variables.py', 'r')
 
+	#inizializzo le variabili del codice morto
 	for line in dead_code_variables:
 		output.write(line)
 
-	output.write('\n\n')
+	output.write('\n')
+
+	#variabile che indica se sono in un blocco di commenti
+	comment = False
+
+	value = ('\t', ' ', '', '\n')
 
 	for line in lines:
 
-		if not '#' in line:
-			if (not line[0] == ' ') and (not line[0] == '\t') and control(line):
-				output.write('\n')
-				insert_dead_code(output)
-				output.write('\n\n' + line)
-			else:
-				output.write(line)
-		
-		'''
-		# verifico che line non sia una riga di commento
-		if line[0] != '#':
+		#Verifico se sto entrando in un blocco di commento
+		if '"""' in line:
+			#se il blocco non inizia e termina sulla stessa riga, allora imposto comment=True per indicare che sono entrato nel blocco,
+			#la variabile comment sarà riportata a False quando verrà trovata la fine del blocco (ovvero la stringa '""""')
+			if line.count('"""') != 2:
+				comment = not comment
+		else:
+			#Se non sono in un blocco di commento
+			if comment == False:
 
-			# se line è vuota e le variabili del codice morto non sono ancora state inizializzate le inizializzo
-			if line == '\n' and not variable_is_inizialized:
-				dead_code_variables = open('./dead_code/dead_code_variables.py', 'r')
+				#verifico se ci sono commenti sulla riga, e in tal caso prendo solo la parte di stringa che lo precede
+				if '#' in line:
+					line = line[:line.find('#')]
 
-				output.write('\n')
+				#verifico che line non sia vuota
+				if line != '':	
+					#verifico che line non inizi con spazi o tabulazioni (ovvero non sia nello scope di un costrutto)
+					if (not line[0] == ' ') and (not line[0] == '\t') and is_candidate(line):
 
-				for line in dead_code_variables:
-					output.write(line)
-					variable_is_inizialized = True
+						#inserisco il codice morto
+						insert_dead_code(output)
+						output.write('\n' + line)
+					
+					#se sono nello scope di un costrutto oppure line non è una riga candidata 
+					else:
 
-				output.write('\n')
+						#verifico che line non sia fatta solo da spazi e tabulazioni
+						if any(c not in value for c in line):
+							#scrivo la line in output
+							output.write(line)
 
-			elif line == '\n':
-
-				insert_dead_code(output)
-				output.write('\n\n')
-
-			else:
-				output.write(line)
-		'''
-
-	output.write('\n')
 	insert_dead_code(output)
-	output.write('\n')
 
 	output.close()
 	source.close()
 
+#funzione che aggiunge codice morto
 def insert_dead_code(output):
-	ran = random.randint(1,15)
+
+	#seglie a random un file tra dead_code_.py1,...,dead_code_21.py
+	ran = random.randint(1,21)
 	dead_code = open('./dead_code/dead_code_' + str(ran) +'.py', 'r')
 
-	output.write('\n')
-
+	#inserisce il file dead_code_x.py nel file output.py
 	for line in dead_code.readlines():
 		output.write(line)
 
 	dead_code.close()
-
-
-
-
-
-
